fe58cc5caec2e3ff54c7c7ef726c689d18259edd
JBR-Sapeta <jakub.ryszard.sapeta@gmail.com>
2024-02-14 23:10:51 +0100

    feat: B-3 configure configService and database connection

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,40 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { SnakeNamingStrategy } from 'typeorm-naming-strategies';
+
+import { ENV_KEYS } from './utils/constants';
+import { DatabaseConfigType } from './utils/types';
+
+import { UserModule } from './user/user.module';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: `.env.${process.env.NODE_ENV}`,
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => {
+        return {
+          type: config.get<DatabaseConfigType>(ENV_KEYS.DB_TYPE),
+          host: config.get<string>(ENV_KEYS.DB_HOST),
+          port: +config.get<string>(ENV_KEYS.DB_PORT),
+          username: config.get<string>(ENV_KEYS.DB_TYPE),
+          password: config.get<string>(ENV_KEYS.DB_TYPE),
+          database: config.get<string>(ENV_KEYS.DB_TYPE),
+          namingStrategy: new SnakeNamingStrategy(),
+          synchronize: process.env.NODE_ENV === 'development',
+          autoLoadEntities: true,
+        };
+      },
+    }),
+    UserModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
