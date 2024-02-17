3169bfaba8b724ef15c263e32922264dd9d51f32
JBR-Sapeta <jakub.ryszard.sapeta@gmail.com>
2024-02-17 21:41:58 +0100

    feat: B-8 enable CORS and add url prefix

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -3,6 +3,16 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.setGlobalPrefix('api/v1');
+
+  app.enableCors({
+    origin: 'http://localhost:3000',
+    methods: ['OPTIONS', 'GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'HEAD'],
+    allowedHeaders: '*',
+    credentials: true,
+  });
+
   await app.listen(8080);
 }
 bootstrap();
