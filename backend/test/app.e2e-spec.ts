2c5fb16f446dff3783c400430a72f00543fdbcdd
JBR-Sapeta <jakub.ryszard.sapeta@gmail.com>
2024-02-17 21:56:31 +0100

    test: B-10 fix e2e test

--- backend/test/app.e2e-spec.ts
+++ backend/test/app.e2e-spec.ts
@@ -16,7 +16,7 @@ describe('AppController (e2e)', () => {
   });
 
   it('/ (GET)', () => {
-    return request(app.getHttpServer()).get('/api/v1/').expect(200);
+    return request(app.getHttpServer()).get('/').expect(200);
   });
 
   afterAll(async () => {
