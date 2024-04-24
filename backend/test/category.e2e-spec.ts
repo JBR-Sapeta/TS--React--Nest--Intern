2386d8ecfd0ef2616ae778a74542e4e5d6e6cad5
JBR-Sapeta <jakub.ryszard.sapeta@gmail.com>
2024-04-24 22:51:18 +0200

    test: B-50 test get category endpoint

---
+++ backend/test/category.e2e-spec.ts
@@ -0,0 +1,96 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { INestApplication } from '@nestjs/common';
+import * as request from 'supertest';
+
+import { AppModule } from './../src/app.module';
+import { CacheService } from './../src/cache/cache.service';
+import { MailService } from './../src/mail/mail.service';
+import { GeocoderService } from './../src/geocoder/geocoder.service';
+
+import { mailService } from './mocks/mail-service';
+import { geocoderService } from './mocks/geocoder-service';
+
+describe('CategoryController (e2e)', () => {
+  let app: INestApplication;
+  let cacheService: CacheService;
+
+  beforeEach(async () => {
+    const moduleFixture: TestingModule = await Test.createTestingModule({
+      imports: [AppModule],
+    })
+      .overrideProvider(MailService)
+      .useValue(mailService)
+      .overrideProvider(GeocoderService)
+      .useValue(geocoderService)
+      .compile();
+
+    app = moduleFixture.createNestApplication();
+
+    cacheService = moduleFixture.get<CacheService>(CacheService);
+
+    await app.init();
+  });
+
+  afterEach(async () => {
+    await cacheService.onApplicationShutdown();
+  });
+
+  afterAll(async () => {
+    await app.close();
+  });
+
+  // ------------------------------------  Helpers  -------------------------------------- \\
+
+  // ------------------------------------  Requests  ------------------------------------- \\
+
+  const sendGetCategoriesRequest = async () => {
+    const response = await request(app.getHttpServer())
+      .get(`/categories`)
+      .send();
+    return response;
+  };
+
+  // --------------------------- GET CATEGORIES - Valid Request -------------------------- \\
+
+  describe('/categories (GET) - Valid Request', () => {
+    it('returns 200 status code', async () => {
+      const response = await sendGetCategoriesRequest();
+      expect(response.status).toBe(200);
+    });
+
+    it('returns success message', async () => {
+      const response = await sendGetCategoriesRequest();
+      expect(response.body.message).toBeTruthy();
+    });
+
+    it('returns proper success response object with payload', async () => {
+      const response = await sendGetCategoriesRequest();
+      expect(Object.keys(response.body)).toEqual([
+        'stausCode',
+        'message',
+        'error',
+        'data',
+      ]);
+    });
+
+    it('returns categories data', async () => {
+      const response = await sendGetCategoriesRequest();
+      expect(Object.keys(response.body.data[0])).toEqual([
+        'id',
+        'name',
+        'parentId',
+        'children',
+      ]);
+    });
+
+    it('returns categories tree', async () => {
+      const response = await sendGetCategoriesRequest();
+      expect(Object.keys(response.body.data[0].children[0])).toEqual([
+        'id',
+        'name',
+        'parentId',
+        'children',
+      ]);
+    });
+  });
+});
