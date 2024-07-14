21d0369f2b1b475016b4e1f0eb786ca355ebb83c
JBR-Sapeta <jakub.ryszard.sapeta@gmail.com>
2024-07-14 15:48:06 +0200

    feat: F-12 register NotFound view and fix box shadow styles

--- frontend/src/router/Router.tsx
+++ frontend/src/router/Router.tsx
@@ -6,13 +6,14 @@ import { Layout } from '@Containers/layout';
 
 import { SignInView } from '@Views/auth';
 import { CompanyListingView } from '@Views/companies';
+import { NotFoundView } from '@Views/errors';
 import { OfferListingView } from '@Views/offers';
 
 import { ROUTER_PATHS } from './constants';
 
 const ROUTER = createBrowserRouter(
   createRoutesFromElements(
-    <Route path="/" element={<Layout />}>
+    <Route path="/" element={<Layout />} errorElement={<NotFoundView />}>
       <Route path={ROUTER_PATHS.COMPANIES} element={<OfferListingView />} />
       <Route path={ROUTER_PATHS.OFFERS} element={<CompanyListingView />} />
       <Route path={ROUTER_PATHS.AUTH} element={<SignInView />} />
